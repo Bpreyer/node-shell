07d60b2faf57b4c1e0705160cbec415dddf0fe0a
Britt Preyer <B.A.Preyerjr@gmail.com>
2020-06-19 12:28:29 -0400

    handing off

---
+++ bash.js
@@ -0,0 +1,16 @@
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// the stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim();
+  // if (cmd === 'pwd') {
+  //   process.stdout.write('You typed: ' + 'hello you are here')
+  //   process.stdout.console.log(`Current directory: ${process.cwd()}`);
+  // }
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+});
+
+
+
